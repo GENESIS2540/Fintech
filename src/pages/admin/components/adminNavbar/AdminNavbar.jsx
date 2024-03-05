2c31e8eb5b95770da16f224665781c96bac17899
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2024-03-05 23:06:17 +0300

    updates: pop over admin

--- src/pages/admin/components/adminNavbar/AdminNavbar.jsx
+++ src/pages/admin/components/adminNavbar/AdminNavbar.jsx
@@ -1,10 +1,49 @@
 import React from "react";
 import logo from "../../assets/svgs/logo.svg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSearch, faUser } from "@fortawesome/free-solid-svg-icons";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
+import { AuditOutlined, HomeOutlined, LogoutOutlined, RetweetOutlined, UserOutlined } from "@ant-design/icons";
+import UuerOutlined from "../../assets/images/check_balance.jpeg";
+import { Popover } from "antd";
 
 const AdminNavbar = () => {
+  const content = (
+    <div className="p-[10px]">
+      <div className="flex gap-[5px] justify-center items-center">
+        <img className="h-[40px] w-[40px]" src={UuerOutlined} alt="" />
+        <div className="font-semibold">
+          <p className="text-[18px]">Hi, Brandon</p>
+          <p>user@gmail.com</p>
+        </div>
+      </div>
+      <hr className="my-[10px]" />
+      <div className="grid gap-[10px]">
+        <div className="flex gap-[5px] opacity-70 transition-all duration-300 ease-in-out hover:opacity-100 cursor-pointer">
+          <AuditOutlined />
+          <p className="">Dashboard</p>
+        </div>
+        <div className="flex gap-[5px] transition-all duration-300 ease-in-out opacity-70 hover:opacity-100 cursor-pointer">
+          <HomeOutlined />
+          <p>Home</p>
+        </div>
+        <div className="flex gap-[5px] transition-all duration-300 ease-in-out opacity-70 hover:opacity-100 cursor-pointer">
+          <UserOutlined />
+          <p>Profile</p>
+        </div>
+        <div className="flex gap-[5px] transition-all duration-300 ease-in-out transition-all duration-300 ease-in-out opacity-70 hover:opacity-100 cursor-pointer">
+          <RetweetOutlined />
+          <p>Update Credentials</p>
+        </div>
+      </div>
+
+      <hr className="my-[10px]" />
+      <div className="flex gap-[5px] cursor-pointer">
+        <LogoutOutlined />
+        <p>Logout</p>
+      </div>
+    </div>
+  );
   return (
     <nav className="grid bg-white p-[10px] gap-[10px] place-content-center grid-cols-2 sm:grid-cols-3 md:h-[12vh] shadow-lg">
       <Link to={"/"} className="flex  items-center gap-[5px] cursor-pointer">
@@ -21,7 +60,13 @@ const AdminNavbar = () => {
         />
       </div>
       <div className="flex place-self-end	 sm:order-last items-center justify-center rounded-[50%] border-[#015FF1] border-2 h-[32px] w-[32px] cursor-pointer ">
-        <FontAwesomeIcon className="text-[20px]" icon={faUser} />
+      <Popover content={content} placement="leftTop" trigger="click">
+      <img
+        className="h-[30px] w-[30px] rounded-[100%] cursor-pointer"
+        src={UuerOutlined}
+        alt=""
+      />
+    </Popover>
       </div>
     </nav>
   );
