ead6e81ca831e60438a553197c1dd7550e9c0652
sheddiey <shadrackcheruiyot429@gmail.com>
2024-03-18 00:05:14 +0300

    added active states

--- src/pages/admin/components/sidebar/Sidebar.jsx
+++ src/pages/admin/components/sidebar/Sidebar.jsx
@@ -17,6 +17,7 @@ const SidebarResponsive = ({
   handleSidebarClick,
   setIsCollapsed,
   isCollapsed,
+  selectedSidebar,
 }) => {
   const [broken, setBroken] = React.useState(
     window.matchMedia("(max-width: 800px)").matches
@@ -29,7 +30,6 @@ const SidebarResponsive = ({
         customBreakPoint="800px"
         onBreakPoint={setBroken}
         toggled={isCollapsed}
-      
         backgroundColor="white"
       >
         <div className="uppercase mt-[10px] mx-[10px] h-[20px] font-semibold text-[11px] font-semibold">
@@ -39,6 +39,7 @@ const SidebarResponsive = ({
         </div>
         <Menu>
           <MenuItem
+            className={selectedSidebar === "dashboard" ? "bg-[#f3f3f3]" : ""}
             onClick={() => handleSidebarClick("dashboard")}
             icon={<FontAwesomeIcon className="text-[16px]" icon={faHome} />}
           >
@@ -46,11 +47,13 @@ const SidebarResponsive = ({
           </MenuItem>
           <MenuItem
             onClick={() => handleSidebarClick("newProduct")}
+            className={selectedSidebar === "newProduct" ? "bg-[#f3f3f3]" : ""}
             icon={<FontAwesomeIcon className="text-[16px]" icon={faBox} />}
           >
             New Product
           </MenuItem>
           <MenuItem
+            className={selectedSidebar === "newCoupon" ? "bg-[#f3f3f3]" : ""}
             onClick={() => handleSidebarClick("newCoupon")}
             icon={<FontAwesomeIcon className="text-[16px]" icon={faGift} />}
           >
@@ -64,12 +67,14 @@ const SidebarResponsive = ({
         </div>
         <Menu>
           <MenuItem
+            className={selectedSidebar === "products" ? "bg-[#f3f3f3]" : ""}
             onClick={() => handleSidebarClick("products")}
             icon={<FontAwesomeIcon className="text-[16px]" icon={faBox} />}
           >
             Products
           </MenuItem>
           <MenuItem
+            className={selectedSidebar === "categories" ? "bg-[#f3f3f3]" : ""}
             onClick={() => handleSidebarClick("categories")}
             icon={
               <FontAwesomeIcon className="text-[16px]" icon={faCubesStacked} />
@@ -78,6 +83,7 @@ const SidebarResponsive = ({
             Categories
           </MenuItem>
           <MenuItem
+            className={selectedSidebar === "collections" ? "bg-[#f3f3f3]" : ""}
             onClick={() => handleSidebarClick("collections")}
             icon={
               <FontAwesomeIcon className="text-[16px]" icon={faBoxesStacked} />
@@ -93,6 +99,7 @@ const SidebarResponsive = ({
         </div>
         <Menu>
           <MenuItem
+            className={selectedSidebar === "orders" ? "bg-[#f3f3f3]" : ""}
             onClick={() => handleSidebarClick("orders")}
             icon={
               <FontAwesomeIcon className="text-[16px]" icon={faCartShopping} />
@@ -108,6 +115,7 @@ const SidebarResponsive = ({
         </div>
         <Menu>
           <MenuItem
+            className={selectedSidebar === "customers" ? "bg-[#f3f3f3]" : ""}
             onClick={() => handleSidebarClick("customers")}
             icon={<FontAwesomeIcon className="text-[16px]" icon={faUsers} />}
           >
@@ -115,15 +123,13 @@ const SidebarResponsive = ({
           </MenuItem>
         </Menu>
         <div className="uppercase mt-[10px] mx-[10px] h-[20px] font-semibold text-[11px] font-semibold">
-          <Typography
-            variant="body2"
-            fontWeight={600}
-          >
+          <Typography variant="body2" fontWeight={600}>
             Promotion
           </Typography>
         </div>
         <Menu>
           <MenuItem
+            className={selectedSidebar === "coupons" ? "bg-[#f3f3f3]" : ""}
             onClick={() => handleSidebarClick("coupons")}
             icon={<FontAwesomeIcon className="text-[16px]" icon={faGift} />}
           >
@@ -138,6 +144,7 @@ const SidebarResponsive = ({
         <div className="pb-[20px]">
           <Menu>
             <MenuItem
+              className={selectedSidebar === "settings" ? "bg-[#f3f3f3]" : ""}
               onClick={() => handleSidebarClick("settings")}
               icon={<FontAwesomeIcon className="text-[16px]" icon={faGear} />}
             >
